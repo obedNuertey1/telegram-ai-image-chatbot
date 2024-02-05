17aa22150d467b28c0fc79600593c449c7df032f
Obed Nuertey <onuertey1997@gmail.com>
2024-02-05 18:03:05 +0000

    $1

--- power-push.bat
+++ power-push.bat
@@ -1,11 +1,5 @@
-# Prompt for commit message
-$commitMessage = Read-Host "Enter commit message"
-
-# Stage all changes
 git add .
 
-# Commit changes with the specified message
-git commit -m $commitMessage
+git commit -m $1
 
-# Push changes to the remote repository
 git push
